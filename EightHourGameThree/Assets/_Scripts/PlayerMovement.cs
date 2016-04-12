41fdd540a54f6e36a369b53b70314c8f63c17b20
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-04-12 13:40:25 -0500

    Looking and Jumping continued

--- EightHourGameThree/Assets/_Scripts/PlayerMovement.cs
+++ EightHourGameThree/Assets/_Scripts/PlayerMovement.cs
@@ -5,9 +5,11 @@ public class PlayerMovement : MonoBehaviour
 {
 
 	public float Velocity;
-
 	public float CameraSensitivity;
+	public float vertRange = 60.0f;
 
+	float VerticalLook = 0;
+	
 	// Use this for initialization
 	void Start()
 	{
@@ -17,8 +19,10 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		float VerticalLook = -Input.GetAxis("Mouse Y") * CameraSensitivity;
-		transform.Rotate(VerticalLook, 0, 0);
+		VerticalLook -= Input.GetAxis("Mouse Y") * CameraSensitivity;
+		VerticalLook = Mathf.Clamp(VerticalLook, -vertRange, vertRange);
+		//transform.Rotate(VerticalLook, 0, 0);
+		Camera.main.transform.localRotation = Quaternion.Euler(VerticalLook, 0, 0);
 
 		float HorizontalLook = Input.GetAxis("Mouse X") * CameraSensitivity;
 		transform.Rotate(0, HorizontalLook, 0);
@@ -27,15 +31,16 @@ public class PlayerMovement : MonoBehaviour
 
 		float StrafeMove = Input.GetAxis("Horizontal") * Velocity;
 
-	//	float VertMove = Input.GetAxis("Jump") * Velocity;
+		//float VertMove = Input.GetAxis("Jump") * Velocity;
 
 		if (Input.GetKey("space"))
 		{
-			gameObject.transform.position = new Vector3(0, Time.deltaTime * Velocity * 5, 0);
-		}
+			Vector3 tempVector = new Vector3(0, Time.deltaTime * Velocity * 5, 0);
+			gameObject.transform.position = gameObject.transform.position + tempVector;
+        }
 
 		Vector3 speed = new Vector3(StrafeMove, 0, ForwardMove);
-		speed = transform.localRotation * speed;
+		speed = transform.rotation * speed;
 
 		CharacterController playerMove = GetComponent<CharacterController>();
 		playerMove.Move(speed);
