e16331947d4be5558b6ef36b06e37edd56878f50
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-04-12 15:01:49 -0500

    Movement and Building
    Fixed most issues with movement using the Character Controller
    Began laying-out and building the scene.

--- EightHourGameThree/Assets/_Scripts/PlayerMovement.cs
+++ EightHourGameThree/Assets/_Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 
 	public float Velocity;
 	public float CameraSensitivity;
-	public float vertRange = 60.0f;
+	public float vertRange;
 
 	float VerticalLook = 0;
 	
@@ -19,30 +19,28 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		VerticalLook -= Input.GetAxis("Mouse Y") * CameraSensitivity;
-		VerticalLook = Mathf.Clamp(VerticalLook, -vertRange, vertRange);
-		//transform.Rotate(VerticalLook, 0, 0);
-		Camera.main.transform.localRotation = Quaternion.Euler(VerticalLook, 0, 0);
-
 		float HorizontalLook = Input.GetAxis("Mouse X") * CameraSensitivity;
 		transform.Rotate(0, HorizontalLook, 0);
 
-		float ForwardMove = Input.GetAxis("Vertical") * Velocity;
-
-		float StrafeMove = Input.GetAxis("Horizontal") * Velocity;
-
-		//float VertMove = Input.GetAxis("Jump") * Velocity;
+		VerticalLook -= Input.GetAxis("Mouse Y") * CameraSensitivity;
+		VerticalLook = Mathf.Clamp(VerticalLook, -vertRange, vertRange);
+		Camera.main.transform.localRotation = Quaternion.Euler(VerticalLook, 0, 0);
 
 		if (Input.GetKey("space"))
 		{
 			Vector3 tempVector = new Vector3(0, Time.deltaTime * Velocity * 5, 0);
 			gameObject.transform.position = gameObject.transform.position + tempVector;
-        }
+		}
 
-		Vector3 speed = new Vector3(StrafeMove, 0, ForwardMove);
+		Vector3 speed = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		speed = transform.rotation * speed;
 
 		CharacterController playerMove = GetComponent<CharacterController>();
-		playerMove.Move(speed);
+		playerMove.SimpleMove(speed);
+
+		if(playerMove.detectCollisions == true)
+		{
+			playerMove.Move(speed);
+		}
 	}
 }
